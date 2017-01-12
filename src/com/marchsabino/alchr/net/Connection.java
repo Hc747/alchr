83cd1faa30c1fcc826990973efce1ad1e20145a3
Hc747 <harrisoncole05@gmail.com>
2017-01-12 12:40:33 +1100

    Removed unnecessary inheritance from URLConnection

--- src/com/marchsabino/alchr/net/Connection.java
+++ src/com/marchsabino/alchr/net/Connection.java
@@ -1,15 +1,13 @@
 package com.marchsabino.alchr.net;
 
-import java.io.IOException;
 import java.net.URL;
-import java.net.URLConnection;
 
 /**
  * Allows a connection between the library and the Old School Runescape Grand Exchange.
  *
  * @author Marcello Sabino
  */
-public class Connection extends URLConnection {
+public class Connection {
 
     /** The charset which the expected response is encoded with */
     public static final String DEFAULT_CHARSET = "UTF-8";
@@ -17,18 +15,21 @@ public class Connection extends URLConnection {
     /** The base URL of Old School Runescape's Grand Exchange. */
     public static final String BASE_URL = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=";
 
+    private final URL url;
+
     /**
      * Constructs a URL connection to the specified URL. A connection to
      * the object referenced by the URL is not created.
      *
      * @param url the specified URL.
      */
+
     public Connection(URL url) {
-        super(url);
-    }
+    	this.url = url;
+	}
 
-    @Override
-    public void connect() throws IOException {
+	public URL getURL() {
+    	return url;
+	}
 
-    }
 }
